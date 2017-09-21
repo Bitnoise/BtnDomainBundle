2455f464dec17b2ca53135608bce6382237bb916
lukasz.d <lukasz.d@bitnoi.se>
2017-09-21 11:27:06 +0200

    composer

--- src/Btn/DomainBundle/BundlesRegistry.php
+++ src/Btn/DomainBundle/BundlesRegistry.php
@@ -3,21 +3,17 @@
 namespace Btn\DomainBundle;
 
 use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
-use Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle;
 use JMS\AopBundle\JMSAopBundle;
 use JMS\DiExtraBundle\JMSDiExtraBundle;
 use JMS\SerializerBundle\JMSSerializerBundle;
 use Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle;
-use Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle;
 use SimpleBus\SymfonyBridge\SimpleBusCommandBusBundle;
 use SimpleBus\SymfonyBridge\SimpleBusEventBusBundle;
-use Symfony\Bundle\DebugBundle\DebugBundle;
 use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
 use Symfony\Bundle\MonologBundle\MonologBundle;
 use Symfony\Bundle\SecurityBundle\SecurityBundle;
 use Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle;
 use Symfony\Bundle\TwigBundle\TwigBundle;
-use Symfony\Bundle\WebProfilerBundle\WebProfilerBundle;
 use Symfony\Component\HttpKernel\Kernel;
 
 /**
@@ -41,7 +37,6 @@ class BundlesRegistry
 
             new SensioFrameworkExtraBundle(),
             new DoctrineBundle(),
-            new DoctrineMigrationsBundle(),
 
             new JMSSerializerBundle(),
             new JMSDiExtraBundle($kernel),
@@ -50,12 +45,6 @@ class BundlesRegistry
             new SimpleBusCommandBusBundle(),
             new SimpleBusEventBusBundle(),
         ];
-
-        if (in_array($kernel->getEnvironment(), ['dev', 'test'], true)) {
-            $bundles[] = new DebugBundle();
-            $bundles[] = new WebProfilerBundle();
-            $bundles[] = new SensioGeneratorBundle();
-        }
 
         return $bundles;
     }
