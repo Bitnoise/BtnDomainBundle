38713ee02732ca2a0f80805560cb284021c4527a
lukasz.d <lukasz.d@bitnoi.se>
2017-09-22 12:41:23 +0200

    event bus

--- src/Btn/DomainBundle/DependencyInjection/Compiler/TraitInjectorMap.php
+++ src/Btn/DomainBundle/DependencyInjection/Compiler/TraitInjectorMap.php
@@ -3,6 +3,7 @@
 namespace Btn\DomainBundle\DependencyInjection\Compiler;
 
 use Btn\Domain\MessageBus\CommandBusAwareTrait;
+use Btn\Domain\MessageBus\EventBusAwareTrait;
 use Symfony\Component\DependencyInjection\ContainerAwareTrait;
 
 class TraitInjectorMap
@@ -12,6 +13,7 @@ class TraitInjectorMap
         return [
             ContainerAwareTrait::class  => ['setContainer', 'service_container'],
             CommandBusAwareTrait::class => ['setCommandBus', 'command_bus'],
+            EventBusAwareTrait::class   => ['setEventBus', 'event_bus'],
         ];
     }
 }
